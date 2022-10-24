a757697dbee5cbc9e8ab3403d8b8ee56eadb12d2
DanielA2079084 <111326945+DanielA2079084@users.noreply.github.com>
2022-10-24 15:26:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,27 +2,51 @@ from ill import parser, interpreter
 
 # debug = True
 
+parser_ = None
+interpreter_ = None
+
+"""
+Compile's source code
+
+Attributes:
+	src: a string consisting the source code
+"""
+def compile(src : str) -> None:
+	interpreter_.interpret(parser_.parse(src))
+
+"""
+Compile's a source file
+
+Attributes:
+	path: a string consisting the path to the source file
+"""
+def compile_file(path : str) -> None:
+	try:
+		with open(path, 'r') as file:
+			compile(file.read())
+			file.close()
+	except FileNotFoundError:
+		print(f"Invalid path '{path}'")
+
+"""
+Main function
+"""
 def main():
+	parser_ = parser()
+	interpreter_ = interpreter()
+	if debug:
+		# path = input("Path: ")
+		path = "tests/hello_world.ill"
+		if path[-4:] != '.ill':
+			print("Invalid file")
+			return
+		compile_file(path)
+		return
 	while True:
-		if debug:
-			# path = input("Path: ")
-			path = "tests/hello_world.ill"
-			if path[-4:] != '.ill':
-				print("Invalid file")
-				continue
-			try:
-				with open(path, "r") as file:
-					ascii_codes = parser(file.read()).parse()
-					interpreter(ascii_codes).interpret()
-					file.close()
-			except FileNotFoundError:
-				print("Invalid path")
-			break
-		else:
-			code = input(":: ")
-			if code == 'exit': break; 
-			ascii_codes = parser(code).parse()
-			interpreter(ascii_codes).interpret()
+		code = input(":: ")
+		if code == 'exit': break; 
+		elif code[-4:] == '.ill': compile_file(code);
+		else: compile(code);
 
 if __name__ == "__main__":
 	main()
