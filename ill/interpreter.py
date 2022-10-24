6bf12c5aa4dbd06a8756c4196b8a4b1722c69286
DanielA2079084 <111326945+DanielA2079084@users.noreply.github.com>
2022-10-24 14:16:09 +0700

    Update interpreter.py

--- ill/interpreter.py
+++ ill/interpreter.py
@@ -1,10 +1,22 @@
 import sys
 
+"""
+Ill's Interpreter
+"""
 class interpreter(object):
 
+	"""
+	Initializes interpreter
+	
+	Attributes:
+		ascii_codes: a list filled with integers
+	"""
 	def __init__(self, ascii_codes : list) -> None:
 		self.ascii_codes = ascii_codes
 
+	"""
+	Interprets ASCII codes
+	"""
 	def interpret(self) -> None:
 		for index, dec in enumerate(self.ascii_codes):
 			# Alter values if not in range of 0 - 255
