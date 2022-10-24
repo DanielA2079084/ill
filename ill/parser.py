89cbefe84d2a96af61f26f8e46d31244ef51b6a0
DanielA2079084 <daniel.2079084@bpkpenabur.sch.id>
2022-10-24 17:41:30 +0700

    updated parser
    - fixed documentation
    - added nodes
    - applied new error system

--- ill/parser.py
+++ ill/parser.py
@@ -1,3 +1,32 @@
+from .error import invalid_char_error
+
+debug = False
+
+"""
+Ill's Parser Node
+"""
+class node:
+
+	"""
+	Initializes Node
+
+	Attributes:
+		file: a string consisting of the file name
+		coloumn: an integer consisting of the value's coloumn position
+		line: an integer consisting of the value's line position
+		value: an integer consisting the ASCII index
+	"""
+	def __init__(self, file : str, coloumn : int, line : int, value : int) -> None:
+		self.coloumn = coloumn
+		self.line = line
+		self.value = value
+
+	"""
+	Represents the node
+	"""
+	def __repr__(self):
+		return f"Node: [Coloumn: {self.coloumn}, Line: {self.line}, Value: {self.value}]"
+
 """
 Ill's Parser
 """
@@ -7,6 +36,13 @@ class parser:
 	Initializes Parser
 	"""
 	def __init__(self) -> None:
+		self.reset_pos()
+
+	"""
+	Resets the parser's position back to default
+	"""
+	def reset_pos(self) -> None:
+		self.coloumn = 0
 		self.line = 1
 		self.pos = -1
 
@@ -15,6 +51,7 @@ class parser:
 	"""
 	def advance(self) -> None:
 		self.pos += 1
+		self.coloumn += 1
 		self.current_char = self.src[self.pos] if self.pos < len(self.src) else None
 
 	"""
@@ -23,23 +60,26 @@ class parser:
 	Attributes:
 		src: a string consisting the source code
 	Result:
-		result: a list filled with integer values
+		result: a list filled with integer / ASCII codes
 	"""
-	def parse(self, src : str) -> list:
+	def parse(self, file_name : str, src : str) -> list:
 		self.src = src
-		result = []
-		first = 65 # Start with the ASCII value of 'A'
-		self.advance() # Initialize self.current_char
+		self.reset_pos()
+		result = []  # The list of ASCII codes
+		first = 65 
+		self.advance()
 		while self.current_char != None:
-			if self.current_char == ' ': # Output
-				result.append(first)
-			elif self.current_char == '\n': # Delimiter
-				pass
-			elif self.current_char == 'I': # Increment
+			if self.current_char == ' ':  # Output
+				result.append(node(file_name, self.coloumn, self.line, first))
+			elif self.current_char == '\n':  # Delimiter
+				# Reset coloumn and add line value since it's a newline character
+				self.coloumn = 1
+				self.line += 1
+			elif self.current_char == 'I':  # Increment
 				first += 1
-			elif self.current_char == 'l': # Decrement
+			elif self.current_char == 'l':  # Decrement
 				first -= 1
-			elif self.current_char == 'i' or self.current_char == 'L': # Multiplication and Division
+			elif self.current_char == 'i' or self.current_char == 'L':  # Multiplication and Division
 				operator = self.current_char
 				self.advance()
 				if (
@@ -47,29 +87,28 @@ class parser:
 					self.current_char != '\n' and
 					self.current_char != 'I' and 
 					self.current_char != 'l'
-				): # Check if next character is not a possible value
+				): 
 					raise Exception("Invalid use of operator")
-				second = 0 # Right node of the operation
+				second = 0  # Right node of the operation
 				while (
 					self.current_char and
 					(
 						self.current_char == 'I' or
 						self.current_char == 'l'
 					)
-				):
+				): 
 					if self.current_char == 'I':
 						second += 1
 					elif self.current_char == 'l':
 						second -= 1
 					self.advance()
-				# if not self.current_char:
-				# 	raise Exception("Invalid value for operation")
-				if operator == 'i': # Multiplication
+				if operator == 'i':  # Multiplication
 					first *= second
-				elif operator == 'L': # Division
+				elif operator == 'L':  # Division
 					first = int(first / second)
 				self.pos -= 1
 			else:
-				raise Exception(f"Invalid character '{self.current_char}'")
+				raise invalid_char_error(self.current_char)
 			self.advance()
+		if debug: print(result);
 		return result
