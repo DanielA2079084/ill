69395850eff6c82a79df36021fd375014c7697d2
DanielA2079084 <daniel.2079084@bpkpenabur.sch.id>
2022-10-24 17:38:48 +0700

    fixed documentation and simplified some code

--- ill/interpreter.py
+++ ill/interpreter.py
@@ -3,26 +3,18 @@ import sys
 """
 Ill's Interpreter
 """
-class interpreter(object):
-
-	"""
-	Initializes interpreter
-	"""
-	def __init__(self):
-		pass
+class interpreter:
 
 	"""
 	Interprets ASCII codes
 	
 	Attributes:
-		ast: a list of integers filled with ASCII codes
+		ast: a list of integers consisting of ASCII codes
 	"""
 	def interpret(self, ascii_codes : list) -> None:
 		for index, dec in enumerate(ascii_codes):
 			# Alter values if not in range of 0 - 255
-			while dec > 255:
-				dec -= 255
-			while dec < 0:
-				dec += 255
-			sys.stdout.write(chr(dec)) # Output converted values
-		print() # \n
+			while dec.value > 255: dec.value -= 255;
+			while dec.value < 0: dec.value += 255;
+			sys.stdout.write(chr(dec.value)) # Output converted values
+		print()
