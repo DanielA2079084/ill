bfae6b4d2427f9b4fb9c5e145c4094f8a8063df7
DanielA2079084 <daniel.2079084@bpkpenabur.sch.id>
2022-10-24 17:42:17 +0700

    fixed documentation and simplified some code

--- main.py
+++ main.py
@@ -1,32 +1,34 @@
-from ill import parser, interpreter
+from ill import invalid_path_error, parser, interpreter
 
-debug = True
+debug = False
 
-parser_ = parser()
-interpreter_ = interpreter()
+parser_ = parser()  # The parser
+interpreter_ = interpreter()  # The interpreter
 
 """
 Compiles source code
 
 Attributes:
-	src: a string consisting the source code
+	file_name: a string consisting of the source file name
+	src: a string consisting of the source code
 """
-def compile(src : str) -> None:
-	interpreter_.interpret(parser_.parse(src))
+def compile(file_name : str, src : str) -> None:
+	interpreter_.interpret(parser_.parse(file_name, src))
 
 """
 Compiles a source file
 
 Attributes:
-	path: a string consisting the path to the source file
+	file_name: a string consisting of the source file name
+	path: a string consisting of the path to the source file
 """
-def compile_file(path : str) -> None:
+def compile_file(file_name : str, path : str) -> None:
 	try:
 		with open(path, 'r') as file:
-			compile(file.read())
+			compile(file_name, file.read())
 			file.close()
 	except FileNotFoundError:
-		print(f"Invalid path '{path}'")
+		invalid_path_error(path)
 
 """
 Main function
@@ -35,18 +37,19 @@ def main():
 	parser_ = parser()
 	interpreter_ = interpreter()
 	if debug:
-		# path = input("Path: ")
-		path = "tests/hello_world.ill"
+		path = input('Path: ')
+		# path = "tests/hello_world.ill"
 		if path[-4:] != '.ill':
-			print("Invalid file")
+			invalid_file_error(path)
 			return
 		compile_file(path)
 		return
 	while True:
-		code = input(":: ")
+		code = input(':: ')
+		if debug: print(f'Source code:\n{code}');
 		if code == 'exit': break; 
-		elif code[-4:] == '.ill': compile_file(code);
-		else: compile(code);
+		elif code[-4:] == '.ill': compile_file(code, code);
+		else: compile('<stdin>', code);
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 	main()
